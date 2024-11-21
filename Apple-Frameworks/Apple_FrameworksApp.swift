7b7ff5703ef3ef3b233e6494b995da6795e9caa3
Ravi Ranjan  Ojha <raviranjanojha999@gmail.com>
2024-11-21 17:52:46 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Ravi Ranjan  Ojha on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
